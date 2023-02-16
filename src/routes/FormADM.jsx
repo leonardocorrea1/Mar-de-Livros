a2fabac0cc5452ff71f6c8f7137c1202317b8b19
Julioxli <julioxlima626@gmail.com>
2023-02-16 17:45:39 -0300

    Merge pull request #4 from Julioxli/revert-2-sign-sigup "Add files via upload"

--- src/routes/FormADM.jsx
+++ src/routes/FormADM.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
 
 const FormADM = () => {
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [error, setError] = useState("");
+
+
+    const navigate = useNavigate();
+
+
+    const Login = (e) => {
+      e.preventDefault();
+        const emailAdm = "julioxlima¨26@gmail.com"
+        const senhaAdm = 12345678
+
+        
+        
+        if (email != emailAdm && senha != senhaAdm) {
+            alert("senha incorreta")
+            navigate("/home")
+        } else {
+            navigate("/homeFunc");
+        }
+
+        
+      };
+
+
+
+
+    
+
+
   return (
-    <div>FormADM</div>
-  )
+    <section className="h-screen">
+  <div className="container px-6 py-12 h-full mt-3">
+    <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-900">
+      <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          className="w-full"
+          alt="Phone image"
+        />
+      </div>
+      <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+      <h1 className="text-center font-normal text-gray-700 mb-5 ">Área Administrador</h1>
+       <form>
+       <div className="mb-6">
+        <input
+          type="email"
+          className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          placeholder="Administrador Email"
+        onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+      </div>
+      <div className="mb-6">
+      <input
+        type="password"
+        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Senha Administrador "
+      onChange={(e) => [setSenha(e.target.value), setError("")]}
+      />
+    </div>
+    <button
+      type="submit"
+      className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+      data-mdb-ripple="true"
+      data-mdb-ripple-color="light"
+      onClick={Login}
+    >
+      Entrar
+    </button>
+       </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+ )
 }
+
 
 export default FormADM
