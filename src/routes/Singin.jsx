79eb00135f256f5719956eae76ef7aa63466b532
Leonardo <47828514+leonardocorrea1@users.noreply.github.com>
2023-02-16 11:55:45 -0300

    Add files via upload

--- src/routes/Singin.jsx
+++ src/routes/Singin.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { useState } from "react"
+import InputForm from "../components/Input/InputForm"
+import ButtonForm from "../components/Button/ButtonForm"
+import { Link, useNavigate } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
+import "./Css/Singin.css"
+
 
 const Singin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate("/home");
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!email | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/addlivros");
+  };
+
+
   return (
-    <div>Singin</div>
+    <div className="container">
+      <label className="label">LOGIN</label>
+      <div className="content">
+      <InputForm className='input-form'
+      type="email"
+      placeholder="Digite seu E-mail"
+      value={email}
+      onChange={(e) => [setEmail(e.target.value), setError("")]}
+      />
+      <InputForm className='input-form'
+      type="password"
+      placeholder="Digite sua senha"
+      value={senha}
+      onChange={(e) => [setSenha(e.target.value), setError("")]} />
+      <label className="labelerror">{error}</label>
+      <ButtonForm text="Entrar" onClick={handleLogin}/>
+      <label className="labelsingin">
+          <p>Não tem uma conta?</p>
+          <p>&nbsp;<Link to="/singnup">Registre-se</Link></p>
+        </label>
+      </div>
+    </div>
   )
 }
 
