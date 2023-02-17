640d6133e7326429b4debfd7d44875372881724b
Júlio Xavier <115498048+Julioxli@users.noreply.github.com>
2023-02-17 10:03:34 -0300

    Update HomeFunc.jsx

--- src/routes/HomeFunc.jsx
+++ src/routes/HomeFunc.jsx
@@ -76,9 +76,9 @@ const ContHome = () => {
             <p>{autor}</p>
             </div>
             <div className="btns">
-              <button><Link to={`/edit/${id}`} >Edit</Link></button>
-              <button><Link to={`/lermais/${id}`} >Ler Mais</Link></button>
-              <button onClick={() => deletelivro(livro.id)}>Delete</button>
+              <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"><Link to={`/edit/${id}`} >Edit</Link></button>
+              <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"><Link to={`/lermais/${id}`} >Ler Mais</Link></button>
+              <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => deletelivro(livro.id)}>Delete</button>
             </div>
             <hr></hr>
           </div>
